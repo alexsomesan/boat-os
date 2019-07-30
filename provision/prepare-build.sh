2697b8cd69f6c449d135eff2d1f4a2f3136bae72
Alex Somesan <alex.somesan@gmail.com>
2019-07-30 12:05:02 +0200

    Don't boot into X by default

--- provision/prepare-build.sh
+++ provision/prepare-build.sh
@@ -3,6 +3,8 @@
 apt-get --allow-releaseinfo-change update
 apt-get upgrade -y
 apt-get install -y build-essential cmake python-pip git libusb-1.0-0-dev pkg-config dnsmasq-base avahi-daemon tmux
+
+systemctl set-default multi-user.target
 systemctl enable avahi-daemon.service
 systemctl enable rtl-ais.service
 systemctl enable kplex.service
